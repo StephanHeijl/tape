7327cbb63609f7e9905356713253582661986c71
sheijl <me@stephanheijl.com>
2020-12-08 16:36:57 +0100

    New mean pooling for BERT

--- tape/models/modeling_bert.py
+++ tape/models/modeling_bert.py
@@ -374,19 +374,24 @@ class ProteinBertPooler(nn.Module):
         if self.trainable_encoder:
             self.dense = nn.Linear(config.hidden_size, config.hidden_size)
         else:
-            # Get three layers at the same time
             self.dense = nn.Linear(config.hidden_size, config.hidden_size)
         self.activation = nn.Tanh()
 
-    def forward(self, hidden_states):
+    def forward(self, hidden_states, attention_mask=None):
         if self.trainable_encoder:
             # We "pool" the model by simply taking the hidden state corresponding
             # to the first token.
             first_token_tensor = hidden_states[:, 0]
             pooled_output = self.dense(first_token_tensor)
         else:
+            if attention_mask is None:
+                attention_mask = torch.ones(hidden_states.shape[:-1])
             # We "pool" the model by mean hidden state
-            mean_token_tensor = hidden_states.mean(dim=1)
+            # We use the attention mask to hide hidden states that do not correspond to a token.
+            div_tensor = attention_mask.float().unsqueeze(-1)
+            sum_token_tensor = (hidden_states * div_tensor).sum(dim=1)
+            mean_token_tensor = sum_token_tensor / div_tensor.sum(dim=1)
+
             pooled_output = self.dense(mean_token_tensor)
 
         pooled_output = self.activation(pooled_output)
@@ -471,7 +476,7 @@ class ProteinBertModel(ProteinBertAbstractModel):
         if len(encoder_outputs) > 1:
             sequence_output = encoder_outputs[1][-1]
 
-        pooled_output = self.pooler(sequence_output)
+        pooled_output = self.pooler(sequence_output, input_ids)
 
         # add hidden_states and attentions if they are here
         outputs = (sequence_output, pooled_output,) + encoder_outputs[1:]
